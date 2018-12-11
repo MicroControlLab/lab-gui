d08f190572d09501f0bb3364e806ea0d5f590a90
Sebastian Weigand <s.weigand.phy@gmail.com>
2018-12-11 20:42:34 +0100

    style: changed the tests to pass ts-lints 'no-unused-expression'

--- tests/base-view.test.tsx
+++ tests/base-view.test.tsx
@@ -6,16 +6,18 @@ describe('testing BaseView', () => {
       const errorMsg =
         'The container selector of test_view needs to match exactly one valid html element. ' +
         'The given value of container is #doesNotExist and matches no element.'
-      expect(() => {
-        new BaseView({ container: '#doesNotExist', name: 'test_view' })
-      }).toThrowError(errorMsg)
+      const exception = () => {
+        const foo = new BaseView({ container: '#doesNotExist', name: 'test_view' })
+      }
+      expect(exception).toThrowError(errorMsg)
     })
     it('error wron type for container', () => {
       const errorMsg =
         'The container of test_view needs to be a querySelector string or a valid html element. The given value was 9001.'
-      expect(() => {
-        new BaseView({ container: 9001 as any, name: 'test_view' })
-      }).toThrowError(errorMsg)
+      const exception = () => {
+        const foo = new BaseView({ container: 9001 as any, name: 'test_view' })
+      }
+      expect(exception).toThrowError(errorMsg)
     })
   })
 })
